0a25995340525d44b1231eca1253729750b2804c
gxliu <gxliu@ikuai8.com>
2018-04-18 11:30:30 +0800

    add lib_learn

---
+++ cpp_learn/lib_learn/main.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+#include <iostream>
+
+int main()
+{
+	hello();
+	return 0 ;
+}
---
+++ cpp_learn/lib_learn/test.h
@@ -0,0 +1,20 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+
+#define HELLO "hello world!"
+#if 0
+class Common {
+	public:
+		Common();
+		void Update();
+		void Loop_handle();
+		void Destroy();
+		void Get_version();
+		void hello();
+};
+
+#endif
+void hello();
+
+#endif
